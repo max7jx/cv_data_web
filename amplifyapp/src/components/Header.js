c546a34c67daf8757456f27d68066c2af31acc5d
Maksym.Shulha <mashulha@deloitte.com>
2023-07-25 20:57:36 +0300

    new header

--- amplifyapp/src/components/Header.js
+++ amplifyapp/src/components/Header.js
@@ -1,59 +1,86 @@
 import React from 'react';
 import { Link } from 'react-scroll';
+import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import '../styles/Header.css'; // Import the custom header styles
+
+const HeaderAppBar = styled(AppBar)(({ theme }) => ({
+  borderBottom: 'none', // Remove the bottom border
+  boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)', // Add a shadow at the bottom
+  padding: '10px 40px', // Add padding to make it narrower
+}));
+
+const SectionLink = styled(Link)(({ theme }) => ({
+  textDecoration: 'none',
+  color: 'inherit',
+  '&:hover': {
+    backgroundColor: 'white', // Show white background on hover
+    borderRadius: '20px', // Add capsule shape
+    transition: 'background-color 0.2s ease', // Add smooth transition
+  },
+  padding: '10px 20px',
+  marginLeft: '10px',
+}));
 
 const Header = () => {
   return (
-    <header className="header"> {/* Use the "header" class for the white background */}
-      <nav className="navbar"> {/* Use the "navbar" class to center the links */}
-        <ul className="nav-list"> {/* Use the "nav-list" class for the list */}
-          <li className="nav-item">
-            <Link
-              to="about"
-              smooth={true}
-              duration={500}
-              className="nav-link" 
-              activeClass="active" 
-            >
-              About Me
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="skills"
-              smooth={true}
-              duration={500}
-              className="nav-link"
-              activeClass="active" 
-            >
-              Skills
-            </Link>
-          </li>
-          {/* <li className="nav-item">
-            <Link
-              to="experience"
-              smooth={true}
-              duration={500}
-              className="nav-link" 
-              activeClass="active" 
-            >
-              Experience
-            </Link>
-          </li> */}
-          <li className="nav-item">
-            <Link
-              to="contact"
-              smooth={true}
-              duration={500}
-              className="nav-link" 
-              activeClass="active" 
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <HeaderAppBar position="static" className="header"> {/* Use the "header" class for the white background */}
+      <Toolbar style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <Typography variant="h6">Your Website Name</Typography>
+        <nav className="navbar"> {/* Use the "navbar" class to center the links */}
+          <ul className="nav-list" style={{ display: 'flex' }}> {/* Use flex to align capsules */}
+            <li className="nav-item">
+              <SectionLink
+                to="about"
+                smooth={true}
+                duration={500}
+                className="nav-link" 
+                activeClass="active" 
+              >
+                About Me
+              </SectionLink>
+            </li>
+            <li className="nav-item">
+              <SectionLink
+                to="skills"
+                smooth={true}
+                duration={500}
+                className="nav-link"
+                activeClass="active" 
+              >
+                Skills
+              </SectionLink>
+            </li>
+            {/* <li className="nav-item" style={{ width: '170px' }}>
+              <SectionLink
+                to="experience"
+                smooth={true}
+                duration={500}
+                className="nav-link" 
+                activeClass="active" 
+              >
+                Experience
+              </SectionLink>
+            </li> */}
+            <li className="nav-item">
+              <SectionLink
+                to="contact"
+                smooth={true}
+                duration={500}
+                className="nav-link" 
+                activeClass="active" 
+              >
+                Contact
+              </SectionLink>
+            </li>
+          </ul>
+        </nav>
+        <IconButton>
+          <ArrowUpwardIcon style={{ fontSize: '2rem' }} />
+        </IconButton>
+      </Toolbar>
+    </HeaderAppBar>
   );
 };
 
